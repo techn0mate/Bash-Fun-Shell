79839b140a0929cab416bedb73817111214b6830
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-05-25 17:29:17 +0530

    mod 2.0

--- fun.sh
+++ fun.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
 # By Soumya Mondal | Under development
+# colors defining
 red='\x1b[38;5;9m'
 yell='\x1b[38;5;216m'
 green='\x1b[38;5;10m'
 purp='\x1b[38;5;171m'
 
-echo Okay testing going on
+echo all under control, Okay?
+
+version='0.0.1'
+mlink='url'
+
+echo Your ip address is $(ifconfig | grep broadcast | awk '{print $2}')
